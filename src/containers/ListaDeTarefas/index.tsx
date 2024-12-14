b13349844fbe8998bc170974fc58873a3da099fa
Rodrigo Bergenthal <rodrigo_bergenthak@live.com>
2024-12-14 03:49:09 -0300

    up arquivos

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,7 +1,27 @@
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+
 const ListaDeTarefas = () => (
-  <main>
-    <p> 2 tarefas marcadas como: "categoria2" e "Termo"</p>
-  </main>
+  <Container>
+    <p>2 tarefas marcadas como: &apos;categoria2&apos; e &apos;Termo&apos;</p>
+    <ul>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+    </ul>
+  </Container>
 )
 
 export default ListaDeTarefas
